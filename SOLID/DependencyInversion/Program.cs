581358d7dd115d0748022db6dcce6fcc273bb49d
vujkovdejan@gmail.com <vujkovdejan@gmail.com>
2021-06-03 22:54:02 +0200

    I D

---
+++ SOLID/DependencyInversion/Program.cs
@@ -0,0 +1,80 @@
+﻿using System.Security;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+
+namespace DependencyInversion
+{
+    // high level parts of the system should not depend on low level parts, both instead should depend of abstraction
+
+    public enum Relationship { Parent, Child, Sibling }
+
+    public class Person
+    {
+        public string Name;
+    }
+
+    // low - level part of the system
+    public class Relationships : IRelationshipBrowser
+    {
+        private List<(Person, Relationship, Person)> relations = new List<(Person, Relationship, Person)>();
+
+        public void AddParentAndChild(Person parent, Person child)
+        {
+            relations.Add((parent, Relationship.Parent, child));
+            relations.Add((child, Relationship.Child, parent));
+        }
+
+        public IEnumerable<Person> FindAllChildrenOf(string name)
+        {
+            foreach (var item in relations.Where(x => x.Item1.Name == name && x.Item2 == Relationship.Parent))
+            {
+                yield return item.Item3;
+            }
+        }
+        // this provides access to low level modules which is bad
+        // public List<(Person, Relationship, Person)> Relations => relations;
+    }
+
+    #region solution
+    public interface IRelationshipBrowser
+    {
+        IEnumerable<Person> FindAllChildrenOf(string name);
+    }
+
+    #endregion
+
+    class Research
+    {
+        // bad example
+        // public Research(Relationships relationships)
+        // {
+        //     // this allows high level module (Research) to access low level one
+        //     var relations = relationships.Relations;
+
+        //     foreach (var item in relations.Where(x => x.Item1.Name == "John" && x.Item2 == Relationship.Parent))
+        //     {
+        //         System.Console.WriteLine($"John has a child called {item.Item3.Name}");
+        //     }
+        // }
+        public Research(IRelationshipBrowser browser)
+        {
+            foreach (var v in browser.FindAllChildrenOf("John"))
+            {
+                System.Console.WriteLine($"John has a child called {v.Name}"); 
+            }
+        }
+        static void Main(string[] args)
+        {
+            var parent = new Person { Name = "John" };
+            var child1 = new Person { Name = "Chris" };
+            var child2 = new Person { Name = "Mary" };
+
+            var relationships = new Relationships();
+            relationships.AddParentAndChild(parent, child1);
+            relationships.AddParentAndChild(parent, child2);
+
+            new Research(relationships);
+        }
+    }
+}
---
+++ SOLID/InterfaceSegregation/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace InterfaceSegregation
+{
+    // interfaces should be segregated so that everyone who implements interface shouldn't have a function that they don't need
+
+    public class Document
+    {
+
+    }
+
+    // bad example
+    public interface IMachine
+    {
+        void Print(Document d);
+        void Scan(Document d);
+        void Fax(Document d);
+
+    }
+
+    // this printer do need every function
+    public class MultiFunctionPrinter : IMachine
+    {
+        public void Fax(Document d)
+        {
+            //
+        }
+
+        public void Print(Document d)
+        {
+            //
+        }
+
+        public void Scan(Document d)
+        {
+            //
+        }
+    }
+
+    // this printer cannot Fax or Scan
+    public class OldFashionedPrinter : IMachine
+    {
+        public void Fax(Document d)
+        {
+            // cannot fax
+        }
+
+        public void Print(Document d)
+        {
+            // can print
+        }
+
+        public void Scan(Document d)
+        {
+            // cannot scan
+        }
+    }
+
+    // solution => break big interface into smaller interfaces
+    public interface IScan
+    {
+        void Scan(Document d);
+    }
+
+    public interface IPrint
+    {
+        void Print(Document d);
+    }
+    public interface IFax
+    {
+        void Fax(Document d);
+    }
+
+    // in case someone needs every function we can create this
+    public interface IBetterMachine : IScan, IPrint, IFax
+    {
+
+    }
+
+    // in case someone needs only particular function, just inherent that one
+    public class OldFashionedPrinter1 : IPrint
+    {
+        public void Print(Document d)
+        {
+            //
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
